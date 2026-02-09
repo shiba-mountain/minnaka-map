f87fc6d66868436a0c1e04dde162c029e5fa561e
shiba-mountain <erk.nishimura@gmail.com>
2026-02-09 12:03:03 +0900

    fix: SPのカルーセルを shadcn に変更 && 変更に伴い、画像表示をPCとSPに分ける

--- frontend/src/components/features/guide-carousel/guide-carousel.tsx
+++ frontend/src/components/features/guide-carousel/guide-carousel.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Autoplay from 'embla-carousel-autoplay'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '~/components/ui/carousel'
 import { guideCarousel } from '~/data/guide-carousel'
 import { useGuideCarousel } from '~/hooks/useGuideCarousel'
 import GuideDescription from './guide-description'
@@ -10,7 +12,6 @@ import GuideStep from './guide-step'
 export default function GuideCarousel() {
   const { activeIndex, startSequenceFrom } = useGuideCarousel()
   const current = guideCarousel[activeIndex]
-  const displayStep = activeIndex + 1
 
   return (
     <>
@@ -26,8 +27,22 @@ export default function GuideCarousel() {
       {/* SP */}
       <div className="md:hidden">
         <GuideHeading />
-        <GuideImage activeIndex={activeIndex} current={current} />
-        <GuideDescription data={current} displayStep={displayStep} />
+        <Carousel
+          opts={{ loop: true }}
+          plugins={[Autoplay({ delay: 5000 })]}
+          className="mx-auto max-w-96"
+        >
+          <CarouselContent>
+            {guideCarousel.map((item, index) => (
+              <CarouselItem key={item.title}>
+                <GuideImage activeIndex={activeIndex} current={item} />
+                <GuideDescription data={item} displayStep={index + 1} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="left-2 top-1/3" />
+          <CarouselNext className="right-2 top-1/3" />
+        </Carousel>
       </div>
     </>
   )
--- frontend/src/components/features/guide-carousel/guide-image.tsx
+++ frontend/src/components/features/guide-carousel/guide-image.tsx
@@ -4,6 +4,7 @@ import type { StepIndex } from '~/data/guide-carousel'
 import type { GuideCarousel } from '~/types/guide-carousel'
 import { AnimatePresence, motion } from 'framer-motion'
 import Image from 'next/image'
+import { useMediaQuery } from '~/hooks/useMediaQuery'
 
 interface Props {
   activeIndex: StepIndex
@@ -11,27 +12,44 @@ interface Props {
 }
 
 export default function GuideImage({ activeIndex, current }: Props) {
+  const isMoblie = useMediaQuery('(max-width: 767px)')
+
   return (
     <div className="px-5 text-center md:px-0">
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={activeIndex}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
-          className="relative inline-block aspect-[2/3] w-full max-w-[280px] overflow-hidden md:aspect-[1/2] md:drop-shadow-xl"
-        >
-          <Image
-            alt={current.title || ''}
-            src={current.imageUrl || ''}
-            fill
-            sizes="(max-width: 767px) 100vw, 280px"
-            priority={activeIndex === 0}
-            className="object-cover object-top"
-          />
-        </motion.div>
-      </AnimatePresence>
+      {isMoblie
+        ? (
+            <div className="relative inline-block aspect-[2/3] w-full max-w-[280px] overflow-hidden">
+              <Image
+                alt={current.title || ''}
+                src={current.imageUrl || ''}
+                fill
+                sizes="(max-width: 767px) 100vw, 280px"
+                priority={activeIndex === 0}
+                className="object-cover object-top"
+              />
+            </div>
+          )
+        : (
+            <AnimatePresence mode="wait">
+              <motion.div
+                key={activeIndex}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.3 }}
+                className="relative inline-block aspect-[1/2] w-full max-w-[280px] drop-shadow-xl"
+              >
+                <Image
+                  alt={current.title || ''}
+                  src={current.imageUrl || ''}
+                  fill
+                  sizes="(max-width: 767px) 100vw, 280px"
+                  priority={activeIndex === 0}
+                  className="object-cover object-top"
+                />
+              </motion.div>
+            </AnimatePresence>
+          )}
     </div>
   )
 }
